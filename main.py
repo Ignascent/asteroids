1b3b4eeb265b37071c98968144fbd221d4748df1
Ignascent <christopher@chris-mutchler.com>
2026-01-31 23:43:52 -0600

    cor: add rotation

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ def main():
     clock = pygame.time.Clock()
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
-        log_state()
+        #log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
